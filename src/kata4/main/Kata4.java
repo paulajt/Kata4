6d9516efdac56e9781b5ff3d0794745d099f48a6
paula <paula@Michica>
2017-11-13 15:44:32 +0000

    Version 2. Kata4

--- src/kata4/main/Kata4.java
+++ src/kata4/main/Kata4.java
@@ -11,11 +11,35 @@ import kata4.view.MailListReader;
 public class Kata4 {
 
     public static void main(String[] args) throws IOException {
-        String filename = "emails.txt";
-        List<String> mailList = MailListReader.read(filename);
-        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
+        
+        Kata4 kata4 = new Kata4();
+        kata4.execute();
+    }
+    
+    private final String filename = "emails.txt";
+    private List<String> mailList;
+    private Histogram<String> histogram;
+    
+    private final void execute() throws IOException {
+        input();
+        process();
+        output();
+    }
+    
+    private void input() throws IOException {
+        mailList = MailListReader.read(filename);
+    }
+    
+    private void process() {
+        histogram = MailHistogramBuilder.build(mailList);
+    }
+    
+    private void output() {
         HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
         histoDisplay.execute();
     }
+    
+    
+    
     
 }
